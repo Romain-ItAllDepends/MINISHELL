4a9edf29b979541985539a12199355bc739187b2
Romain Gobet <rgobet@1E7.42angouleme.fr>
2024-07-23 14:43:38 +0200

    Little norme!

--- src/utils/split_quote_utils.c
+++ src/utils/split_quote_utils.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 09:08:50 by rgobet            #+#    #+#             */
-/*   Updated: 2024/07/23 12:13:58 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/07/23 14:41:46 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ void	verif_multi_quote(t_char_list **tmp_char, char **quote,
 		*quote_in_var = state;
 	if (state != *quote_in_var)
 	{
-		// 	*quote_in_var = TRUE;
 		special_case_quote(tmp_char, quote, i);
 		if (*quote_in_var == FALSE)
 			*quote_in_var = is_not_in_quote(*quote, *tmp_char);
